3b879c1655555bdc3ffd93525aca00a5bc23a820
Dwardrip <130064505+Dwardrip@users.noreply.github.com>
2023-04-27 15:32:04 +0200

    added forecast w/o API

--- index.js
+++ index.js
@@ -40,6 +40,31 @@ h3.innerHTML = `${hours}:${minutes}`;
 
 //
 
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-3">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="http://openweathermap.org/img/wn/50d@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 18° </span>
+          <span class="weather-forecast-temperature-min"> 12° </span>
+        </div>
+      </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+ 
+}
+
 function displayWeather(response) {
  
   celsiusTemperature = response.data.main.temp;
